55c7533d18e567c1bbc55377417aa6877cabf0ee
acquia-service-acc-user <joyce.bansode@acquia.com>
2023-12-13 22:17:09 +0530

    hide edit component

--- gitmod/matomo.js
+++ gitmod/matomo.js
@@ -11,7 +11,7 @@ function hideThings () {
   if ((webIdeButton = document.querySelector('[data-qa-selector="action_dropdown"]'))) {
     webIdeButton.style.display = 'none';
   }
-  if ((editButton = document.getElementById('[id="dropdown-toggle-btn-102"]'))) {
+  if ((editButton = document.getElementById('[id="dropdown-toggle-btn-54"]'))) {
     editButton.style.display = 'none';
   }
   if ((editIdeButton = document.querySelector('[data-track-action="click_edit_ide"]'))) {
